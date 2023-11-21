926b475c4ae6eca774d48df3375f9d939a043591
Tyler Seiford <50031864+TylerSeiford@users.noreply.github.com>
2023-11-20 20:28:42 -0600

    Build out robot container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,7 +1,21 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.PowerDistribution;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+
 public class RobotContainer {
+    private static final class Constants {
+        private static final int CONTROLLER_PORT = 0;
+
+        private static final int PDP_CAN_ID = 1;
+    }
+
+    private final CommandXboxController controller = new CommandXboxController(Constants.CONTROLLER_PORT);
+    private final PowerDistribution pdp;
+
     public RobotContainer() {
+        pdp = new PowerDistribution(Constants.PDP_CAN_ID, PowerDistribution.ModuleType.kCTRE);
+
         configureBindings();
     }
 
