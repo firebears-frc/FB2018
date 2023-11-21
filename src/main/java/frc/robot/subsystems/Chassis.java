24e76eb2582efb0abf3610c40a0f584ae548cb5a
Tyler Seiford <50031864+TylerSeiford@users.noreply.github.com>
2023-11-21 16:37:43 -0600

    Revert to simple drivetrain

--- src/main/java/frc/robot/subsystems/Chassis.java
+++ src/main/java/frc/robot/subsystems/Chassis.java
@@ -4,13 +4,11 @@ import java.util.function.Supplier;
 
 import org.littletonrobotics.junction.AutoLogOutput;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
-import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
-import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -20,45 +18,27 @@ public class Chassis extends SubsystemBase {
         private static final int REAR_LEFT_CAN_ID = 3;
         private static final int FRONT_RIGHT_CAN_ID = 4;
         private static final int REAR_RIGHT_CAN_ID = 5;
-
-        private static final double TRACK_WIDTH = Units.inchesToMeters(30); // TODO!
     }
 
     private final WPI_TalonSRX frontLeft = new WPI_TalonSRX(Constants.FRONT_LEFT_CAN_ID);
     private final WPI_TalonSRX rearLeft = new WPI_TalonSRX(Constants.REAR_LEFT_CAN_ID);
     private final WPI_TalonSRX frontRight = new WPI_TalonSRX(Constants.FRONT_RIGHT_CAN_ID);
     private final WPI_TalonSRX rearRight = new WPI_TalonSRX(Constants.REAR_RIGHT_CAN_ID);
-    private final DifferentialDriveKinematics kinematics = new DifferentialDriveKinematics(Constants.TRACK_WIDTH);
+    private final MotorControllerGroup left = new MotorControllerGroup(frontLeft, rearLeft);
+    private final MotorControllerGroup right = new MotorControllerGroup(frontRight, rearRight);
+    private final DifferentialDrive drive = new DifferentialDrive(left, right);
 
     private ChassisSpeeds target = new ChassisSpeeds();
-
-    public Chassis() {
-        // TODO: Configure TalonSRXes for PID velocity control
-    }
 
     private void drive(ChassisSpeeds speeds) {
         target = speeds;
 
-        DifferentialDriveWheelSpeeds wheelSpeeds = kinematics.toWheelSpeeds(target);
-        frontLeft.set(ControlMode.Velocity, wheelSpeeds.leftMetersPerSecond);
-        rearLeft.set(ControlMode.Velocity, wheelSpeeds.leftMetersPerSecond);
-        frontRight.set(ControlMode.Velocity, wheelSpeeds.rightMetersPerSecond);
-        rearRight.set(ControlMode.Velocity, wheelSpeeds.rightMetersPerSecond);
+        drive.arcadeDrive(speeds.vxMetersPerSecond, speeds.omegaRadiansPerSecond, false);
     }
 
     @AutoLogOutput
     public ChassisSpeeds targetSpeeds() {
         return target;
-    }
-
-    public DifferentialDriveWheelSpeeds wheelSpeeds() {
-        return new DifferentialDriveWheelSpeeds(frontLeft.getSelectedSensorVelocity(),
-                frontRight.getSelectedSensorVelocity());
-    }
-
-    @AutoLogOutput
-    public ChassisSpeeds actualSpeeds() {
-        return kinematics.toChassisSpeeds(wheelSpeeds());
     }
 
     public Command defaultCommand(Supplier<ChassisSpeeds> supplier) {
